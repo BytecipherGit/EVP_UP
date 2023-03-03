3cdccdd2a7a03e358be2b8c76596a5b24c325142
ravindra yadav <ravindrayadav6016th@gmail.com>
2023-03-03 16:40:21 +0530

    Add Admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'role' => 'admin',
+            'name' => 'admin',
+            'email' => 'admin3@gmail.com',
+            'password' => Hash::make('123456'),
+            'org_name' =>'ByteCipher',
+            'org_web'=>'bytecipher.net',
+            'designation'=>'Developer',
+            'department'=>'CS',
+            'address'=>'Indore',
+            'country'=>'India',
+            'state'=>'Madhya Pradesh',
+            'city'=>'Indore',
+            'pin'=>'482003'
+        ]);
+
+        User::create([
+            'role' => 'superadmin',
+            'name' => 'admin',
+            'email' => 'superadmin@gmail.com',
+            'password' => Hash::make('123456'),
+            'org_name' =>'ByteCipher',
+            'org_web'=>'bytecipher.net',
+            'designation'=>'Developer',
+            'department'=>'CS',
+            'address'=>'Indore',
+            'country'=>'India',
+            'state'=>'Madhya Pradesh',
+            'city'=>'Indore',
+            'pin'=>'482003'
+        ]);
+    }
+}
