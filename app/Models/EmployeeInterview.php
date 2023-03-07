1b3f120f6c44d904156ce959ff20a1fed572c584
tiwarilalit601 <tiwarilalit601@gmail.com>
2023-03-07 18:09:27 +0530

    worked on phone template

--- app/Models/EmployeeInterview.php
+++ app/Models/EmployeeInterview.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class InterviewEmployees extends Model
+class EmployeeInterview extends Model
 {
     use HasFactory, SoftDeletes;
     protected $table='interview_employees';
